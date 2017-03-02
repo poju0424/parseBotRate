bc19465ad3324b51d747bb2dd6985a1420428199
pojulee <poju0424@gmail.com>
2017-03-02 15:27:17 +0800

    test

--- createTable.py
+++ createTable.py
@@ -1,7 +1,10 @@
 #executeQuery("CREATE TABLE IF NOT EXISTS users (sourceName varchar(255), sourceID varchar(255) UNIQUE, sourceType varchar(255) UNIQUE, tick timestamp, linkUser varchar(255), openAutoReply integer NOT NULL, Primary Key (sourceID, sourceType))")
-import mysql.connector
+import psycopg2
 
-cnx = mysql.connector.connect(user='byfozfzreatyuo', password='8f54e0b9274e32cefa7c7610ce7b6c4226397338e8cf7bed6892624c97a2c699',host='ec2-54-163-236-33.compute-1.amazonaws.com',database='ddgd2hokh5t1r')
-cursor = cnx.cursor()
-insertDB = ("CREATE TABLE IF NOT EXISTS users (sourceName varchar(255), sourceID varchar(255) UNIQUE, sourceType varchar(255) UNIQUE, tick timestamp, linkUser varchar(255), openAutoReply integer NOT NULL, Primary Key (sourceID, sourceType))")
-cursor.execute(insertDB)
+#cnx = mysql.connector.connect(user='byfozfzreatyuo', password='8f54e0b9274e32cefa7c7610ce7b6c4226397338e8cf7bed6892624c97a2c699',host='ec2-54-163-236-33.compute-1.amazonaws.com',database='ddgd2hokh5t1r')
+conn = psycopg2.connect("dbname=ddgd2hokh5t1r user=byfozfzreatyuo password=8f54e0b9274e32cefa7c7610ce7b6c4226397338e8cf7bed6892624c97a2c699 host=ec2-54-163-236-33.compute-1.amazonaws.com ")
+cursor = conn.cursor()
+# insertDB = ("CREATE TABLE IF NOT EXISTS users (sourceName varchar(255), sourceID varchar(255) UNIQUE, sourceType varchar(255) UNIQUE, tick timestamp, linkUser varchar(255), openAutoReply integer NOT NULL, Primary Key (sourceID, sourceType))")
+# cursor.execute(insertDB)
+cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+conn.commit()
