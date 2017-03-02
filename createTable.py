cba0dcf0abb66696647bddc9a56826690bdb57be
pojulee <poju0424@gmail.com>
2017-03-02 15:20:12 +0800

    again

---
+++ createTable.py
@@ -0,0 +1,7 @@
+#executeQuery("CREATE TABLE IF NOT EXISTS users (sourceName varchar(255), sourceID varchar(255) UNIQUE, sourceType varchar(255) UNIQUE, tick timestamp, linkUser varchar(255), openAutoReply integer NOT NULL, Primary Key (sourceID, sourceType))")
+import mysql.connector
+
+cnx = mysql.connector.connect(user='byfozfzreatyuo', password='8f54e0b9274e32cefa7c7610ce7b6c4226397338e8cf7bed6892624c97a2c699',host='ec2-54-163-236-33.compute-1.amazonaws.com',database='ddgd2hokh5t1r')
+cursor = cnx.cursor()
+insertDB = ("CREATE TABLE IF NOT EXISTS users (sourceName varchar(255), sourceID varchar(255) UNIQUE, sourceType varchar(255) UNIQUE, tick timestamp, linkUser varchar(255), openAutoReply integer NOT NULL, Primary Key (sourceID, sourceType))")
+cursor.execute(insertDB)
