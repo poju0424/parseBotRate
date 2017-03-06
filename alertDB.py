67c50d98f3f71fb85186745ea147b4091f9be518
pojulee <poju0424@gmail.com>
2017-03-06 16:23:46 +0800

    again

--- alertDB.py
+++ alertDB.py
@@ -1,5 +1,6 @@
 import psycopg2
 import urlparse
+import os
 
 tableName = ["bot_AUD", "bot_CAD", "bot_CHF", "bot_CNY", "bot_EUR", "bot_GBP", "bot_HKD", "bot_IDR", "bot_JPY", "bot_KRW", "bot_MYR", "bot_NZD", "bot_PHP", "bot_SEK", "bot_SGD", "bot_THB", "bot_USD", "bot_VND", "bot_ZAR"]
 # conn = psycopg2.connect("dbname=ddgd2hokh5t1r user=byfozfzreatyuo password=8f54e0b9274e32cefa7c7610ce7b6c4226397338e8cf7bed6892624c97a2c699 host=ec2-54-163-236-33.compute-1.amazonaws.com ")
