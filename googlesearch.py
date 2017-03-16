042d206c3a1a7c6b04bf8337e3b0214705dc3f0e
pojulee <poju0424@gmail.com>
2017-03-16 08:56:33 +0800

    test for flask

--- googlesearch.py
+++ googlesearch.py
@@ -1,4 +1,16 @@
-def app(environ, start_response):
+from google import google
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Vultr!'
+
+def search(keyWord):
+    num_page = 1
+    search_results = google.search(keyword, num_page)
+
+def app1(environ, start_response):
     data = b"Hello, World!\n"
     start_response("200 OK", [
         ("Content-Type", "text/plain"),
