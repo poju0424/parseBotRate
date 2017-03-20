0342734b2d016dca2528c8d3a3b96a4424274dc4
pojulee <poju0424@gmail.com>
2017-03-20 14:56:34 +0800

    tttt

--- fetchRateFromTxt.py
+++ fetchRateFromTxt.py
@@ -1,13 +1,15 @@
 import urllib2
 import re
 
+def CheckString(input):
+    regex = r"^[A-Z][A-Z][A-Z]"
+    matches = re.finditer(regex, input)
+    return matches
+
 filePath = "http://rate.bot.com.tw/xrt/fltxt/0/day"
 data = urllib2.urlopen(filePath)
 
 for line in data:
-    print check_string(line)
+    print CheckString(line)
 
-def check_string(input):
-    regex = r"^[A-Z][A-Z][A-Z]"
-    matches = re.finditer(regex, input)
-    return matches
+
