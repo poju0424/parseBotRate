2b66ffe292fde98d4fc3f8e4d25969f325758496
pojulee <poju0424@gmail.com>
2017-03-20 15:33:54 +0800

    ttt

--- fetchRateFromTxt.py
+++ fetchRateFromTxt.py
@@ -1,17 +1,29 @@
 import urllib2
 import re
+import urlparse
+import cgi
+
+logging.basicConfig()
+fileName = ""
+
+def GetFileName(url):
+    response = urllib2.urlopen(url)
+    _, params = cgi.parse_header(response.headers.get('Content-Disposition', ''))
+    return params['filename']
 
 def CheckString(input):
     regex = r"^[A-Z][A-Z][A-Z]"
     matches = re.match(regex, input)
     return matches
 
-filePath = "http://rate.bot.com.tw/xrt/fltxt/0/day"
-data = urllib2.urlopen(filePath)
+def FetchRate():
+    global fileName
+	
+    filePath = "http://rate.bot.com.tw/xrt/fltxt/0/day"
+	print GetFileName(filePath)
+    data = urllib2.urlopen(filePath)
+    bankName = "bot"
 
-for line in data:
-    if CheckString(line):
-        print line.split()
-    
-
-
+    for line in data:
+        if CheckString(line):
+            print line.split()
