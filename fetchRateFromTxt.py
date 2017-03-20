8cc338d396d1fe7e511724f0e30fd9fc8852a453
pojulee <poju0424@gmail.com>
2017-03-20 15:02:44 +0800

    ddd

--- fetchRateFromTxt.py
+++ fetchRateFromTxt.py
@@ -10,6 +10,8 @@ filePath = "http://rate.bot.com.tw/xrt/fltxt/0/day"
 data = urllib2.urlopen(filePath)
 
 for line in data:
-    print CheckString(line)
+    if match:
+        print process(match)
+    #print CheckString(line)
 
 
